292ba3014f12bd3a90396ebbe7d10fa7f3b6ec74
Marcelo Laprea <30522943+marcelolaprea@users.noreply.github.com>
2018-01-21 12:41:21 +0100

    Models created

---
+++ HastenProject/HastenProject/Models/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  HastenProject
+//
+//  Created by Marcelo Laprea on 1/21/18.
+//  Copyright © 2018 Marcelo. All rights reserved.
+//
+
+import Foundation
+
+struct Player{
+    let name:String?
+    let surname:String?
+    let imageURL:String?
+}
---
+++ HastenProject/HastenProject/Models/Sport.swift
@@ -0,0 +1,15 @@
+//
+//  Sport.swift
+//  HastenProject
+//
+//  Created by Marcelo Laprea on 1/21/18.
+//  Copyright © 2018 Marcelo. All rights reserved.
+//
+
+import Foundation
+
+struct Sport{
+    let type:String?
+    let title:String?
+    var players:[Player]?
+}
